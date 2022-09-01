96169f4f626aa3d578baa9498c7e789081879e24
Tsedal Lombebo <Brightlink01@gmail.com>
2022-09-01 08:01:16 +0000

    task 3 The BaseModel complete

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
     def __str__(self):
         '''string formating function'''
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
