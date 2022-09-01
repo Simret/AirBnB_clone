cb0a24563b8ec78f0c93623bda18e2e738e7ede2
Tsedal Lombebo <Brightlink01@gmail.com>
2022-09-01 08:59:35 +0000

    task 4 is complete

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ import models
 
 class BaseModel():
     ''' This class is base model for airBnB console project'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+            for k, val in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    self.__dict__[k] = datetime.strptime(val, timeformat)
+                else:
+                    self.__dict__[k] = val
+
     def __str__(self):
         '''string formating function'''
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
