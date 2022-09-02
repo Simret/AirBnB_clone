0594227fa3b4d1a8e467a66eee1d0ae922745540
Tsedal Lombebo <Brightlink01@gmail.com>
2022-09-02 20:09:00 +0000

    manadator task complete

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines the State class."""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+
+    """Represent a state.
+
+    Attributes:
+        name string data type: It is name of the state.
+    """
+
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""User class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    
+    """Represent a User.
+    Attributes:
+        email type string: email of the users
+        password string data type: password of the user.
+        first_name string data type: The first name of the user.
+        last_name : The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
